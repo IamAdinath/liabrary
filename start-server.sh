d6ca0c0961f1ca377ca23bfd8b94bbf89137c277
IamAdinath <adinathgore.17@gmail.com>
2023-04-24 16:01:07 +0900

    add bash script to start nginx server

---
+++ start-server.sh
@@ -0,0 +1,5 @@
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
+    (cd liabrary; python manage.py createsuperuser --no-input)
+fi
+(gunicorn liabrary.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
+nginx -g "daemon off;"
